d855f8eb09c9cb4b3b2e4e72fed0b174300c517a
Huynh Van Thien <thienhuynh2605@gmail.com>
2025-10-27 15:36:57 +0700

    add augmentation

--- model.py
+++ model.py
@@ -52,7 +52,7 @@ def FireB(x, s, e):
 def build_model(input_shape=(64,64,1), num_classes=8):
     inputs = Input(shape=input_shape)
 
-    # x = RandomFlip("horizontal")(inputs)
+    x = RandomFlip("horizontal")(inputs)
     # x = RandomRotation(0.1)(x)      
     # x = RandomZoom(0.1)(x)              
     # x = RandomTranslation(0.1,0.1)(x)   
@@ -60,7 +60,7 @@ def build_model(input_shape=(64,64,1), num_classes=8):
     # x = RandomBrightness(0.1)(x)  
 
     # First Conv
-    x = Conv2D(32, (3,3), padding="same")(inputs)
+    x = Conv2D(32, (3,3), padding="same")(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
