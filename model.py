0243c553a8f12d653eb3a297c2d72ac0d0e0b02f
Huynh Van Thien <thienhuynh2605@gmail.com>
2025-10-27 16:08:44 +0700

    modify conv, add augmentation

--- model.py
+++ model.py
@@ -53,14 +53,14 @@ def build_model(input_shape=(64,64,1), num_classes=8):
     inputs = Input(shape=input_shape)
 
     x = RandomFlip("horizontal")(inputs)
-    # x = RandomRotation(0.1)(x)      
+    x = RandomRotation(0.1)(x)      
     # x = RandomZoom(0.1)(x)              
     # x = RandomTranslation(0.1,0.1)(x)   
     # x = RandomContrast(0.1)(x)      
     # x = RandomBrightness(0.1)(x)  
 
     # First Conv
-    x = Conv2D(32, (3,3), padding="same")(x)
+    x = Conv2D(64, (3,3), padding="same")(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
@@ -74,7 +74,7 @@ def build_model(input_shape=(64,64,1), num_classes=8):
     x = Dropout(0.2)(x)
 
     # Final conv layers
-    x = Conv2D(32, (3,3), padding="same")(x)
+    x = Conv2D(64, (3,3), padding="same")(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
